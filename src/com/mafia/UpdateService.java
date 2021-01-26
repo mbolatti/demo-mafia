3556b9ee2c97aad32e339b3a4bab60cc6b178327
mbolatti <martinbolatti@gmail.com>
2021-01-26 14:02:32 +0100

    Mafia survelliance organization manager

--- src/com/mafia/UpdateService.java
+++
@@ -1,9 +0,0 @@
-package com.mafia;
-
-/**
- * Service to mantain the Mafia structure
- */
-
-public class UpdateService {
-    // tener un servicio para marcar los que tienen  mas de 50 subordinados.
-}
